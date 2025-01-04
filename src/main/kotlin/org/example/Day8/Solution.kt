7ab72b9a4af35cf40a482cb37e8e2ed77eecfd29
Ivan <vaano94@gmail.com>
2025-01-04 11:57:12 +0100

    Day 8 - second part

--- src/main/kotlin/org/example/Day8/Solution.kt
+++ src/main/kotlin/org/example/Day8/Solution.kt
@@ -1,22 +1,34 @@
 package org.example.Day8
 
+import java.io.File
 import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.Path
+
 
 fun main() {
 //    val input = """
-//        ............
-//        ........0...
-//        .....0......
-//        .......0....
-//        ....0.......
-//        ......A.....
-//        ............
-//        ............
-//        ........A...
-//        .........A..
-//        ............
-//        ............
+//        ...................
+//        ...................
+//        ...................
+//        .......YyYyY.......
+//        ...................
+//        .......R...S.......
+//        ......r.....s......
+//        ......R.....S......
+//        .....ra.....ts.....
+//        .......a...t.......
+//        ...................
+//        ...................
+//        ...................
+//        ...................
+//        ...................
+//        ...................
+//        ...................
+//        .................B.
+//        ................B..
+//    """.trimIndent()
+//    val input = """
+//        ..........T..T.......
 //    """.trimIndent()
 //    println(input)
     val input = readInput()
@@ -26,7 +38,73 @@ fun main() {
     placeAntinodes(modifiableArray)
     printMap(modifiableArray)
 
+    countAntinodesOnEveryAntenna(modifiableArray)
+
 }
+
+fun countAntinodesOnEveryAntenna(antennaArray: Array<CharArray>) {
+    // find first # index
+    val uniqueAntennaNodes = mutableListOf<AntinodeLocation>()
+
+    // collect all the possible combinations of letters
+    for (x in antennaArray.indices) {
+        for (y in 0..< antennaArray[x].size) {
+                // for every next # found
+                if (antennaArray[x][y] != '.') {
+                    uniqueAntennaNodes.add(AntinodeLocation(x, y))
+            }
+        }
+    }
+    println(uniqueAntennaNodes)
+
+    val uniqueAntinodesOverall = mutableSetOf<AntinodeLocation>()
+
+    while (uniqueAntennaNodes.isNotEmpty()) {
+        val firstAntenna = uniqueAntennaNodes.removeFirst()
+        uniqueAntinodesOverall.add(AntinodeLocation(firstAntenna.x, firstAntenna.y))
+        // go backward and forward until end of array for the distance
+        uniqueAntennaNodes.forEach { nextAntinode ->
+            countAntennasOnPath(antennaArray, firstAntenna, nextAntinode, uniqueAntinodesOverall)
+        }
+    }
+    println("Second solution")
+    println(uniqueAntinodesOverall.size)
+}
+
+fun countAntennasOnPath(antennaArray: Array<CharArray>, firstAntenna: AntinodeLocation, nextAntinode: AntinodeLocation, uniqueAntinodesOverall: MutableSet<AntinodeLocation>) {
+    // first pair
+    // println("Coorinates # are $firstAntenna")
+    val newXa = firstAntenna.x - nextAntinode.x
+    val newYa = firstAntenna.y - nextAntinode.y
+
+    countOnPathFor(antennaArray, firstAntenna, newXa, newYa, uniqueAntinodesOverall)
+
+    // second pair, other direction
+    val newXb = nextAntinode.x - firstAntenna.x
+    val newYb = nextAntinode.y - firstAntenna.y
+
+    countOnPathFor(antennaArray, firstAntenna, newXb, newYb, uniqueAntinodesOverall)
+}
+
+fun countOnPathFor(
+    antennaArray: Array<CharArray>,
+    antinode: AntinodeLocation,
+    diffX: Int,
+    diffY: Int,
+    uniqueAntinodesOverall: MutableSet<AntinodeLocation>
+) {
+    var continuingX = antinode.x + diffX
+    var continuingY = antinode.y + diffY
+    while (continuingX >= 0 && continuingX < antennaArray.size && continuingY >= 0 && continuingY < antennaArray[0].size) {
+        if (antennaArray[continuingX][continuingY] != '.') {
+            // it's a an antenna, and should count
+            uniqueAntinodesOverall.add(AntinodeLocation(continuingX, continuingY))
+        }
+        continuingX += diffX
+        continuingY += diffY
+    }
+}
+
 
 fun placeAntinodes(antennaArray: Array<CharArray>) {
     val antinodeSet = mutableSetOf<AntinodeLocation>()
@@ -44,7 +122,7 @@ fun placeAntinodes(antennaArray: Array<CharArray>) {
     }
 
     // group nodes by letter
-    val groupedNodes = uniqueAntennaNodes.groupBy { it.name }
+    val groupedNodes = uniqueAntennaNodes.groupBy { it.name }.filter { it.value.size > 1 }
 //    println(groupedNodes)
 
     // for each combination, place node on the map
@@ -59,44 +137,67 @@ fun placeAntinodes(antennaArray: Array<CharArray>) {
                 // first pair
                 val newXa = firstNode.x - pairedNode.x
                 val newYa = firstNode.y - pairedNode.y
-                drawAntinode(antennaArray, firstNode.x, firstNode.y, NodeDifference(newXa, newYa), antinodeSet)
+                val wasSetFirstNode =
+                    drawAntinode(antennaArray, firstNode.x, firstNode.y, NodeDifference(newXa, newYa), antinodeSet)
 
                 // second pair, other direction
                 val newXb = pairedNode.x - firstNode.x
                 val newYb = pairedNode.y - firstNode.y
-                drawAntinode(antennaArray, pairedNode.x, pairedNode.y, NodeDifference(newXb, newYb), antinodeSet)
+                val wasSetSecondNode =
+                    drawAntinode(antennaArray, pairedNode.x, pairedNode.y, NodeDifference(newXb, newYb), antinodeSet)
+
+                if (wasSetFirstNode || wasSetSecondNode) {
+                    // if at least one of the antinodes was set, both antennas counts as antinodes
+                    antinodeSet.add(AntinodeLocation(firstNode.x, firstNode.y))
+                    antinodeSet.add(AntinodeLocation(pairedNode.x, pairedNode.y))
+                }
 
             }
         }
     }
-
 
     // we want to have UNIQUE results. Even if potential antinode location combine with results of MULTIPLE other Antennas
     println("Antinodes counted: ${antinodeSet.count()}")
 
 }
 
-fun drawAntinode(antennaArray: Array<CharArray>, startX: Int, startY: Int, diff: NodeDifference, antinodeSet: MutableSet<AntinodeLocation>) {
+fun drawAntinode(
+    antennaArray: Array<CharArray>,
+    startX: Int,
+    startY: Int,
+    diff: NodeDifference,
+    antinodeSet: MutableSet<AntinodeLocation>
+): Boolean {
     // get initial cords
     var continuingX = startX + diff.difX
     var continuingY = startY + diff.difY
 
+    var antinodeWasSet = false
+
     // when can draw, draw and update cords with the diff
-    if ( continuingX >= 0 && continuingX < antennaArray.size && continuingY >= 0 && continuingY < antennaArray[0].size) {
+    while (continuingX >= 0 && continuingX < antennaArray.size && continuingY >= 0 && continuingY < antennaArray[0].size) {
         if (antennaArray[continuingX][continuingY] != '#' && antennaArray[continuingX][continuingY] != '.') {
             antinodeSet.add(AntinodeLocation(continuingX, continuingY))
-            return
+            continuingX += diff.difX
+            continuingY += diff.difY
+            continue
+        }
+        if (antennaArray[continuingX][continuingY] == '#') {
+            // antinode was set and counted, skipping computation
+            continuingX += diff.difX
+            continuingY += diff.difY
+            continue
         }
         if (antennaArray[continuingX][continuingY] == '.') { // not busy and can draw
             antennaArray[continuingX][continuingY] = '#'
             antinodeSet.add(AntinodeLocation(continuingX, continuingY))
             continuingX += diff.difX
             continuingY += diff.difY
-//            printMap(antennaArray)
+            antinodeWasSet = true
+
         }
     }
-    return
-
+    return antinodeWasSet
 }
 
 fun printMap(antennaArray: Array<CharArray>) {
@@ -117,7 +218,8 @@ data class AntennaLocation(val name: Char, val x: Int, val y: Int)
 data class AntinodeLocation(val x: Int, val y: Int)
 
 fun readInput(): String {
-    val lines = Files.readString(Paths.get(object{}.javaClass.getResource("/Day8/input.txt")?.path ?: "")).trimIndent()
+    val path: Path = File(object{}.javaClass.getResource("/Day8/input.txt")?.file ?: "").toPath()
+    val lines = Files.readString(path).trimIndent()
     return lines
 }
 
