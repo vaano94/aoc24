2d3ae6cb70dfb449b6b6791b7b5a1fc958c73c96
vaano94 <vaano94@gmail.com>
2025-04-21 14:45:50 +0200

    Day 11 - second part

--- src/main/kotlin/org/example/Day11/Solution.kt
+++ src/main/kotlin/org/example/Day11/Solution.kt
@@ -6,73 +6,54 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 val currentYear = BigInteger.valueOf(2024L)
+var cache = mutableMapOf<Pair<BigInteger, Int>, BigInteger>()
 
 fun main() {
 
-//    val input = readInput()
-    val input = "125 17"
+    val input = readInput()
 
-//    val blinkingStonesArray = iterateStones(input, 25)
-//    println(blinkingStonesArray.size)
+    val blinkingStonesArray = iterateStones(input, 25)
+    println(blinkingStonesArray.size)
 
-    // todo would make sense to split it into equal chunks or smth, and use arrays
-    // todo instead of joining lists
-    val blinkingStonesArrayPartTwo = iterateStonesPart2("125", 75)
-    // todo wrong 1036288 0 72 20 24 4048 1 4048 8096 28 67 60 32 13
-
-//    val blinkingStonesArrayPartTwo = iterateStonesPart2("512 72 2024 2 0 2 4 2867 6032 9", 4)
-//    println(blinkingStonesArrayPartTwo)
-
-//    val processSingleStoneElement = processSingleStoneElement("125 17", 1)
-//    println("Final")
-//    println(processSingleStoneElement)
+    val iterateStonesPart2 = iterateStonesPart2(input, 75)
+    println(iterateStonesPart2)
 
 }
 
 fun iterateStonesPart2(input: String, times: Int): BigInteger {
     // todo iterate element one by one instead of iterating list as a whole
     val singleStones = input.split(" ")
-    val sum = singleStones.parallelStream().map { stone ->
-        processSingleStoneElement(stone, times)
-    }
-        .peek(::print)
-        .map { it.trim().split(" ").size }
-//        .peek(::println)
-        .toList()
-        .sum()
-//        .reduce((0))
-//    return sum.get()
-    return sum.toBigInteger()
+    val sum = singleStones
+        .map { stone ->
+            countContribution(stone.toBigInteger(), times)
+    }.reduce(BigInteger::plus)
+    return sum
 }
 
-fun processSingleStoneElement(stone: String, times: Int): String {
-    // 10
-    // 1 0
-    // 2024 1
-    // 20 24 2024
-    // 2 0 2 4 20 24
-
-    var acc: String = ""
+fun countContribution(stone: BigInteger, times: Int): BigInteger {
     if (times == 0) {
-//        println(stone)
-        return BigInteger.valueOf(stone.toLong()).toString() + " "
+        return BigInteger.ONE
     }
 
-    repeat(times) {
-//        println("Times ${times}")
-        if (stone.toBigInteger() == BigInteger.ZERO) {
-            return processSingleStoneElement("1", times - 1)
-        } else if (BigInteger(stone).toString().length % 2 == 0)
-            return processSingleStoneElement(
-                stone.substring(0, stone.length / 2),
-                times - 1
-            ) + processSingleStoneElement(stone.substring(stone.length / 2, stone.length), times - 1)
-        else {
-            return processSingleStoneElement(stone.toBigInteger().times(currentYear).toString(), times - 1)
-        }
+    val maybeCached = cache[stone to times]
+    if (maybeCached != null) {
+//        println("Found cache value: $maybeCached")
+        return maybeCached
     }
-    return stone
+
+    val result: BigInteger = when {
+        stone == BigInteger.ZERO -> countContribution(BigInteger.ONE, times -1)
+        stone.toString().length % 2 == 0 -> {
+            val s = stone.toString()
+            val mid = s.length / 2
+            countContribution(s.substring(0, mid).toBigInteger(), times - 1) + countContribution(s.substring(mid).toBigInteger(), times - 1)
+        }
+        else -> countContribution(stone.times(currentYear), times - 1)
+    }
+    cache.putIfAbsent(stone to times, result)
+    return result
 }
+
 
 fun iterateStones(input: String, times: Int): List<String> {
     var resultingArray: List<String> = input.split(" ")
@@ -87,9 +68,6 @@ fun iterateStones(input: String, times: Int): List<String> {
                 else listOf((stone.toBigInteger().times(BigInteger.valueOf(2024L))).toString())
             }
             .flatten()
-
-//        println(resultingArray)
-
     }
     return resultingArray
 }
